120051cfc8a591a2cd2648e5c5968544a3e4905e
Ieuan-Steel11 <ieumansteel@gmail.com>
2022-09-07 03:48:45 +0100

    added types folder

---
+++ first_project/target/debug/deps/first_project-371b1a186439dde9.d
@@ -0,0 +1,5 @@
+/home/ieuan/Code/rust/first_project/target/debug/deps/first_project-371b1a186439dde9.rmeta: src/main.rs
+
+/home/ieuan/Code/rust/first_project/target/debug/deps/first_project-371b1a186439dde9.d: src/main.rs
+
+src/main.rs:
---
+++ vars_mut/target/debug/deps/vars_mut-4b1e26e3d747deee.d
@@ -0,0 +1,5 @@
+/home/ieuan/Code/rust/vars_mut/target/debug/deps/vars_mut-4b1e26e3d747deee.rmeta: src/main.rs
+
+/home/ieuan/Code/rust/vars_mut/target/debug/deps/vars_mut-4b1e26e3d747deee.d: src/main.rs
+
+src/main.rs:
