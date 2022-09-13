15a3aa52f407c7c43a5c9995933642edae85cafa
Ieuan-Steel11 <ieumansteel@gmail.com>
2022-09-13 02:37:16 +0100

    if expressions

--- control_flow/src/main.rs
+++ control_flow/src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    if_expr(6);
+}
+
+fn if_expr(num: i32) {
+    if num < 5 {
+        println!("condition true")
+    } else {
+        println!("condition false")
+        // all other conditions
+    }
+
+    if num != 0 {
+        println!("if num isn't 0")
+        // if num won't work because rust won't try to convert to bool from non-bool
+    }
+
 }
